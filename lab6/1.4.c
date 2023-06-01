5b4cd52bcd3bdfcca5218cc8255af4ddc871b181
kovriginaa <80074121+kovriginaa@users.noreply.github.com>
2023-06-01 20:01:58 +0300

    6

---
+++ lab6/1.4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+extern char **environ;
+int main(int argc, char *argv[])
+{
+  char **p;
+  int count = 0;
+  for (p = environ; *p != NULL; p++)
+  { /* перебор всех элементов массива */
+    count += 1;
+    printf("%s\n", *p); 
+    if (count == argv[1])
+     { 
+	break;
+     }
+  }
+}
