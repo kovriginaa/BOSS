d364b9e180f0cada05aa35d8101cc7987248e589
kovriginaa <80074121+kovriginaa@users.noreply.github.com>
2023-06-01 20:03:56 +0300

    6

---
+++ lab6/2.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+  int pid = fork();
+ 
+  if (pid == 0) {
+    printf("Это сообщение из дочернего процесса\n");
+    printf("PID %d PPID %d\n", getpid(), getppid());
+  } else if (pid > 0) {
+    printf("Это сообщение из родительского процесса.\n"
+           "Идентификатор дочернего процесса:  %d\n", pid);
+  }
+  sleep(10);
+
+  return 0;
+}
